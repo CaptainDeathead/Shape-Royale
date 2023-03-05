c211d0a779a2a610c0261e644144081512b4a8f3
CaptainDeathead <115883954+CaptainDeathead@users.noreply.github.com>
2023-03-05 12:52:05 +0800

    binded quit button to index.html

--- Game/Scripts/Main_Menu.gd
+++ Game/Scripts/Main_Menu.gd
@@ -4,6 +4,9 @@ func _on_Ai_pressed():
 	get_tree().change_scene("res://Scenes/Ai.tscn")
 
 func _on_Quit_pressed():
+	# load the index.html file
+	var url = OS.get_executable_path().get_base_dir().plus_file("index.html")
+	OS.shell_open(url)
 	get_tree().quit()
 
 var players = ["Square", "Triangle", "Circle"]
